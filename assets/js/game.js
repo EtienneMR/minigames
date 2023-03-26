8309c881994de0bb32d275b79e2030b6942a50ad
EtienneMR <jeu.etiennemr@gmail.com>
2023-03-26 12:33:13 +0200

    Added offset and full-size

--- assets/js/game.js
+++ assets/js/game.js
@@ -105,6 +105,7 @@ jQuery(() => {
                 video.loop = true
                 video.controls = false
                 video.src = message
+
                 video.play()
                 messageDiv.append(video)
                 break;
@@ -130,55 +131,92 @@ jQuery(() => {
             else {
                 chat.removeAttr("data-show")
             }
+
+            chatHistory.css("max-height", "")
         }, 5000)
+
+        setTimeout(() => {
+            if (messageDiv.clientHeight > $(window).height() * 0.25) {
+                chatHistory.css("max-height", `${messageDiv.clientHeight + 16}px`)
+            }
+        }, 500)
 
         chatHistory.scrollTop(chatHistory[0].scrollHeight)
     })
+
+    let loading = false
+    let controller = null
+
+    function load(offset = 0) {
+        if (controller && !controller.signal.aborted) controller.abort()
+        controller = new AbortController()
+
+        loading = true
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=vqt2EIdJmahElquY7qTlzHzvJ5vmNEXu&q=${encodeURIComponent(chatInput.val())}&limit=15&offset=0&rating=r&lang=fr$&offset=${offset}`, {signal: controller.signal})
+            .then(res => res.json())
+            .then(res => {
+                if (!offset) {
+                    chatGifs.scrollLeft(0)
+                    chatGifs.children().remove()
+                }
+
+                res.data.reduce((promise, gif) => {
+                    let images = gif.images
+
+                    let video = document.createElement("video")
+                    video.muted = true
+                    video.controls = false
+                    video.crossOrigin = ""
+                    video.src = images.fixed_height.mp4
+
+                    video.addEventListener('mouseover', () => video.play(), false)
+                    video.addEventListener("click", () => {
+                        socket.emit("chat", {
+                            type: "gif",
+                            message: images.original.mp4
+                        })
+                        chatInput.val("")
+                        chatInput.trigger("input")
+                    }, { once: true })
+
+                    chatGifs.append(video)
+
+                    return promise.then(() => new Promise((resolve) => {
+                        if (video && video.parentElement) {
+                            video.addEventListener("ended", resolve, { once: true })
+                            video.play()
+                        }
+                        else resolve()
+                    }))
+                }, Promise.resolve())
+
+                loading = false
+            })
+            .catch(err => {
+                if (!(err instanceof Error && err.name == "AbortErr")) {
+                    console.error(err)
+                }
+            })
+    }
 
     chatInput.on("input", (evt) => {
         if (chatInput.val()) {
             chat.attr("data-gifs", true)
 
-            fetch(`https://api.giphy.com/v1/gifs/search?api_key=vqt2EIdJmahElquY7qTlzHzvJ5vmNEXu&q=${encodeURIComponent(chatInput.val())}&limit=15&offset=0&rating=g&lang=fr`)
-                .then(res => res.json())
-                .then(res => {
-                    chatGifs.children().remove()
-                    console.log(res)
-                    res.data.reduce((promise, gif) => {
-                        let images = gif.images
-
-                        let video = document.createElement("video")
-                        video.muted = true
-                        video.controls = false
-                        video.src = images.fixed_height.mp4
-
-                        video.addEventListener('mouseover', () => video.play(), false)
-                        video.addEventListener("click", () => {
-                            socket.emit("chat", {
-                                type: "gif",
-                                message: images.original.mp4
-                            })
-                            chatInput.val("")
-                            chatInput.trigger("input")
-                        }, { once: true })
-
-                        chatGifs.append(video)
-
-                        return promise.then(() => new Promise((resolve) => {
-                            if (video && video.parentElement) {
-                                video.addEventListener("ended", resolve, { once: true })
-                                video.play()
-                            }
-                            else resolve()
-                        }))
-                    }, Promise.resolve())
-                })
+            load()
         }
         else {
             chat.removeAttr("data-gifs")
             let children = chatGifs.children()
 
             setTimeout(() => children.remove(), 1000)
+        }
+    })
+
+    chatGifs.on("scroll", () => {
+        if (chatGifs[0].scrollWidth - (chatGifs[0].scrollLeft + chatGifs[0].clientWidth) < 50 && !loading && chat.attr("data-gifs")) {
+            console.log("loading more")
+            load(chatGifs.children().length)
         }
     })
 
